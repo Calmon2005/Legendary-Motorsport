5389d8ab1077e82db2651e4eb2d40e11e5f36c53
Calmon2005 <mcalmonpereira@gmail.com>
2025-11-15 15:22:01 -0300

    atualizado

--- produto.php
+++ produto.php
@@ -17,14 +17,16 @@ session_start();
 
     <form class="caixa" action="compra.php" method="POST">
             <img class="imagestyle" src="igm\488-Spider-Dianteira-2-scaled.jpg" alt="488 spider">
-        <p id="prc">300000.00 </p>
+        <p>R$ 300.000.00 </p>
+        <input type="hidden" name="prc" value="300000.00">
         <button>Comprar</button>
     </form>
     
     <form class="caixa" action="compra.php" method="POST">
             <img class="imagestyle" src= "igm\FERRARI-812-GTS-DIANTEIRA-ESQUERDA.webp" alt="812 gts">
         <h2>Ferrari 812 GTS</h2>
-        <p id="prc">200000.00</p>
+        <p>R$ 200.000.00</p>
+        <input type="hidden" name="prc" value="200000.00">
         <button>Comprar</button>
     </form>
 
