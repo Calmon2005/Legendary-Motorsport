3e3be69fd0a0feffea405893ed2fea138ca25fde
Calmon2005 <mcalmonpereira@gmail.com>
2025-11-15 13:29:31 -0300

    tela inicial com o php para garantir que o usuario continue logado

---
+++ prj.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Legendary Motorsport</title>
+    <link rel="stylesheet" href="prj.css">
+</head>
+<body>
+
+    <!-- ÁREA INICIAL -->
+    <section id="inicio">
+    <h1>Legendary Motorsport <?php echo $_SESSION["usuario"]["nome"]; ?></h1>
+    <p>Os veículos mais lendários do mundo estão aqui.</p>
+    <h2>R$ <?php echo $_SESSION["usuario"]["saldo"]; ?></h2>
+    <button id="btnCadastro">Ir para o Cadastro</button>
+    <button id="btnCarros">Carros</button>
+
+</section>
+<script src="prj.js"></script>
+</body>
+</html>
