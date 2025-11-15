b94e8eccd058c7336a7d8ff7f68d7a8883d1d105
Calmon2005 <mcalmonpereira@gmail.com>
2025-11-15 15:04:26 -0300

    logica da compra adicionada

---
+++ compra.php
@@ -0,0 +1,20 @@
+<?php
+session_start(); // INICIA A SESSÃO
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $preco = $_POST["prc"];
+    $saldo = $_SESSION["usuario"]["saldo"];
+    $compra = $saldo - $preco;
+    $id = $_SESSION["usuario"]["id"];
+if ($saldo<$preco) {
+$conexao = new mysqli('localhost', 'root', '', 'motorsport', 3307);
+
+    if ($conexao->connect_error) {
+        die("Erro de conexão: " . $conexao->connect_error);
+    }
+$_SESSION["usuario"]["saldo"] = $compra;
+$sql = "UPDATE usuarios SET saldo = $compra WHERE id =$id";
+}
+else {
+    echo "<script>alert('Saldo insuficiente!');</script>";
+}
+}
