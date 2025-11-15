bccbc25e1ebba225fad01a38fa48094992112848
Calmon2005 <mcalmonpereira@gmail.com>
2025-11-15 13:25:14 -0300

    javascript adicionado

---
+++ prj.js
@@ -0,0 +1,23 @@
+// --- BOTÃO "Ir para Cadastro" (só existe no index.html) ---
+const btnCadastro = document.getElementById('btnCadastro');
+if (btnCadastro) {
+    btnCadastro.addEventListener('click', () => {
+        window.location.href = "cadastro.html";
+    });
+}
+
+// --- BOTÃO "Ver Carros" (se você usar no index.html) ---
+const btnCarros = document.getElementById('btnCarros');
+if (btnCarros) {
+    btnCarros.addEventListener('click', () => {
+        window.location.href = "produtos.html"; 
+    });
+}
+
+// --- BOTÃO VOLTAR (só existe no cadastro.html) ---
+const vlt = document.getElementById('vlt');
+if (vlt) {
+    vlt.addEventListener('click', () => {
+        window.location.href = "prj.html";
+    });
+}
