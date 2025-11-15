9a66e1746d84956a4bef44802f68baa59b31a844
Jorge <jorgejapao00@gmail.com>
2025-11-15 16:36:56 -0300

    alert updated to return to products page

--- compra.php
+++ compra.php
@@ -5,18 +5,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $saldo = $_SESSION["usuario"]["saldo"];
     $compra = $saldo - $preco;
     $id = $_SESSION["usuario"]["id"];
-if ($saldo>$preco) {
-$conexao = new mysqli('localhost', 'root', '', 'motorsport', 3307);
+    if ($saldo>=$preco) {
+        $conexao = new mysqli('localhost', 'root', '', 'motorsport', 3306);
+        $_SESSION["usuario"]["saldo"] = $compra;
+        $sql = "UPDATE usuarios SET saldo = $compra WHERE id =$id";
+        $conexao->query($sql);
+        echo "<script>alert('Compra realizada!'); window.location.href='produto.php';</script>";
 
     if ($conexao->connect_error) {
         die("Erro de conexão: " . $conexao->connect_error);
     }
-$_SESSION["usuario"]["saldo"] = $compra;
-$sql = "UPDATE usuarios SET saldo = $compra WHERE id =$id";
-$conexao->query($sql);
-}
-else {
-    echo "<script>alert('Saldo insuficiente!');</script>";
-}
+    }
+    else {
+        echo "<script>alert('Saldo insuficiente! Seu saldo atual é: ' + $saldo); window.location.href='produto.php';</script>";
+    }
 }
 ?>
