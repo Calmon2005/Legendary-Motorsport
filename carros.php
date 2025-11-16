6ec189c0bde15a415482ab0106209a827a0fa985
Jorge <jorgejapao00@gmail.com>
2025-11-16 10:47:11 -0300

    cars array added

---
+++ carros.php
@@ -0,0 +1,29 @@
+<?php
+$carros = [
+    [
+        "nome" => "Ferrari 488 Spider",
+        "preco" => 300000.00,
+        "img" => "igm/488-Spider-Dianteira-2-scaled.jpg"
+    ],
+    [
+        "nome" => "Ferrari 812 GTS",
+        "preco" => 200000.00,
+        "img" => "igm/FERRARI-812-GTS-DIANTEIRA-ESQUERDA.webp"
+    ],
+    [
+        "nome" => "Porsche GTS",
+        "preco" => 500000.00,
+        "img" => "igm/6765028_0_W_969e12f697.jpg"
+    ],
+    [
+        "nome" => "Maserati Folgore",
+        "preco" => 700000.00,
+        "img" => "igm/granturismo-folgore.webp"
+    ],
+    [
+        "nome" => "Porsche Macan",
+        "preco" => 700000.00,
+        "img" => "igm/granturismo-folgore.webp"
+    ]
+];
+?>
