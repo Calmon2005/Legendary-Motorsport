a0c388efd224aca08eee69349a79f97040a5ad43
Jorge <jorgejapao00@gmail.com>
2025-11-16 10:08:21 -0300

    main page updated with welcome message

--- prj.php
+++ prj.php
@@ -9,17 +9,20 @@ session_start();
     <title>Legendary Motorsport</title>
     <link rel="stylesheet" href="prj.css">
 </head>
+
 <body>
 
     <!-- ÁREA INICIAL -->
     <section id="inicio">
-    <h1>Legendary Motorsport <?php echo $_SESSION["usuario"]["nome"]; ?></h1>
-    <p>Os veículos mais lendários do mundo estão aqui.</p>
-    <h2>R$ <?php echo $_SESSION["usuario"]["saldo"]; ?></h2>
-    <button id="btnCadastro">Ir para o Cadastro</button>
-    <button id="btnCarros">Carros</button>
+        <h1>Legendary Motorsport</h1>
+        <p>Os veículos mais lendários do mundo estão aqui.</p>
+        <h2>Bem vindo(a), <?php echo $_SESSION["usuario"]["nome"]; ?></h2>
+        <h2>R$ <?php echo $_SESSION["usuario"]["saldo"]; ?></h2>
+        <button id="btnCadastro">Ir para o Cadastro</button>
+        <button id="btnCarros">Carros</button>
+    </section>
 
-</section>
+
 <script src="prj.js"></script>
 </body>
 </html>
